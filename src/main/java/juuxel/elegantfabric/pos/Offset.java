f44a85affe33a3e65c77385b064ad5ebd26072a7
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-02-01 11:47:45 +0200

    Make ConstantOffset the only offset pos and rename it to Offset

--- src/main/java/juuxel/elegantfabric/pos/Offset.java
+++ src/main/java/juuxel/elegantfabric/pos/Offset.java
@@ -3,36 +3,42 @@ package juuxel.elegantfabric.pos;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import org.cactoos.Scalar;
+import org.cactoos.scalar.Unchecked;
 
-public final class Offset implements Scalar<BlockPos> {
-    private final Scalar<BlockPos> pos;
-    private final Scalar<Direction> direction;
-    private final int amount;
-
-    public Offset(Scalar<BlockPos> pos, Scalar<Direction> direction, int amount) {
-        this.pos = pos;
-        this.direction = direction;
-        this.amount = amount;
+public final class Offset extends BlockPos {
+    public Offset(BlockPos origin, Direction direction) {
+        this(origin, direction, 1);
     }
 
-    public Offset(BlockPos pos, Direction direction, int amount) {
-        this(() -> pos, () -> direction, amount);
+    public Offset(Scalar<BlockPos> origin, Scalar<Direction> direction) {
+        this(new Unchecked<>(origin).value(), new Unchecked<>(direction).value());
     }
 
-    public Offset(Scalar<BlockPos> pos, Direction direction) {
-        this(pos, () -> direction, 1);
+    public Offset(BlockPos origin, Scalar<Direction> direction) {
+        this(origin, new Unchecked<>(direction).value());
     }
 
-    public Offset(BlockPos pos, Scalar<Direction> direction) {
-        this(() -> pos, direction, 1);
+    public Offset(Scalar<BlockPos> origin, Direction direction) {
+        this(new Unchecked<>(origin).value(), direction);
     }
 
-    public Offset(BlockPos pos, Direction direction) {
-        this(() -> pos, () -> direction, 1);
+    public Offset(Scalar<BlockPos> origin, Scalar<Direction> direction, int amount) {
+        this(new Unchecked<>(origin).value(), new Unchecked<>(direction).value(), amount);
     }
 
-    @Override
-    public BlockPos value() throws Exception {
-        return new BlockPos(pos.value().offset(direction.value(), amount));
+    public Offset(BlockPos origin, Scalar<Direction> direction, int amount) {
+        this(origin, new Unchecked<>(direction).value(), amount);
+    }
+
+    public Offset(Scalar<BlockPos> origin, Direction direction, int amount) {
+        this(new Unchecked<>(origin).value(), direction);
+    }
+
+    public Offset(BlockPos origin, Direction direction, int amount) {
+        super( // Slightly more optimized than `super(origin.offset(direction, amount));`
+            origin.getX() + direction.getOffsetX() * amount,
+            origin.getY() + direction.getOffsetY() * amount,
+            origin.getZ() + direction.getOffsetZ() * amount
+        );
     }
 }
